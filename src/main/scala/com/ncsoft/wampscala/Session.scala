d58f8ad964412d6d70e3b1425bc296a0886f88cc
Justin Choi <justin@bytecodelab.com>
2016-01-18 13:46:48 +0900

    ...

--- src/main/scala/com/ncsoft/wampscala/Session.scala
+++ src/main/scala/com/ncsoft/wampscala/Session.scala
@@ -5,15 +5,15 @@ import java.util.Date
 import akka.actor.ActorRef
 import scala.collection.mutable
 
-trait BaseSession {
-  def id:Long
-  def realm:String
-
-  var peerActor:ActorRef = null
-  var outActor:ActorRef = null
-}
+//trait BaseSession {
+//  def id:Long
+//  def realm:String
+//
+//  var peerActor:ActorRef = null
+//  var outActor:ActorRef = null
+//}
 
 case class Session(id:Long, realm:String, router:ActorRef, client:ActorRef,
                    subscriptionIds:mutable.Set[Long], registrationIds:mutable.Set[Long])
 
-case class SimpleSession(id:Long, realm:String, createdAt:Date) extends BaseSession
+//case class SimpleSession(id:Long, realm:String, createdAt:Date) extends BaseSession
